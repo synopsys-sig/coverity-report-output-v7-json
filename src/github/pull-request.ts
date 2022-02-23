73b477bdb77e5cd199fe612cf3bc179a7bbd5d06
Richard Otte <rotte@blackducksoftware.com>
2022-02-23 17:07:25 -0500

    feat: Parsing what we want out of a PR diff

--- src/github/pull-request.ts
+++ src/github/pull-request.ts
@@ -1,6 +1,6 @@
 import {context, getOctokit} from '@actions/github'
 import {GITHUB_TOKEN} from '../inputs'
-import {getPullRequestNumber, getSha} from './github-context'
+import {getPullRequestNumber} from './github-context'
 
 export async function getPullRequestDiff(): Promise<GitDiff[]> {
   const octokit = getOctokit(GITHUB_TOKEN)
@@ -8,7 +8,11 @@ export async function getPullRequestDiff(): Promise<GitDiff[]> {
   const pullRequestNumber = getPullRequestNumber()
 
   if (!pullRequestNumber) {
-    return Promise.reject('Could not get Pull Request Diff: Action was not running on a Pull Request')
+    return Promise.reject(
+      Error(
+        'Could not get Pull Request Diff: Action was not running on a Pull Request'
+      )
+    )
   }
 
   const response = await octokit.rest.pulls.get({
@@ -16,17 +20,54 @@ export async function getPullRequestDiff(): Promise<GitDiff[]> {
     repo: context.repo.repo,
     pull_number: pullRequestNumber,
     mediaType: {
-      format: "diff",
-    },
+      format: 'diff'
+    }
   })
 
-  const gitDiff = response.data
+  const diffs = []
 
-  return Promise.reject('Not yet implemented')
+  const diff = response.data as unknown as string
+  const diffLines = diff.split('\n')
+
+  let path = undefined
+  for (const line of diffLines) {
+    if (line.startsWith('diff --git')) {
+      // TODO: Handle spaces in path
+      path = line.split(' ')[2].substring(2)
+    }
+
+    if (line.startsWith('@@')) {
+      let changedLines = line.substring(3)
+      changedLines = changedLines.substring(0, changedLines.indexOf(' @@'))
+
+      const linesAddedPosition = changedLines.indexOf('+')
+      if (linesAddedPosition > -1) {
+        // We only care about the right side because Coverity can only analyze what's there, not what used to be --rotte FEB 2022
+        const linesAddedString = changedLines.substring(linesAddedPosition + 1)
+        const separatorPosition = linesAddedString.indexOf(',')
+
+        const startLine = parseInt(
+          linesAddedString.substring(0, separatorPosition)
+        )
+        const lineCount = parseInt(
+          linesAddedString.substring(separatorPosition + 1)
+        )
+        const endLine = startLine + lineCount - 1
+
+        diffs.push({
+          filePath: path,
+          firstLine: startLine,
+          lastLine: endLine
+        })
+      }
+    }
+  }
+
+  return diffs
 }
 
 export interface GitDiff {
-  filePath: string,
-  firstLine: number,
+  filePath?: string
+  firstLine: number
   lastLine: number
 }
