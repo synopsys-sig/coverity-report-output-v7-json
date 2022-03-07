4c6f33c3ef68dc8b7f74905c7757d8e206f55a18
Gavin Killough <gavink@synopsys.com>
2022-03-07 10:54:46 -0500

    Add Coverity credential inputs

--- src/inputs.ts
+++ src/inputs.ts
@@ -2,3 +2,5 @@ import {getInput} from '@actions/core'
 
 export const GITHUB_TOKEN = getInput('github-token')
 export const JSON_FILE_PATH = getInput('json-file-path')
+export const COVERITY_USERNAME = getInput('coverity-username')
+export const COVERITY_PASSWORD = getInput('coverity-password')
