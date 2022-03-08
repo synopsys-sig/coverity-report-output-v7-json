b96c78a8322c9d6f321c32aebbd84c3dff5b6d74
Gavin Killough <gavink@synopsys.com>
2022-03-08 10:13:51 -0500

    Include coverity project name as input

--- dist/index.js
+++ dist/index.js
@@ -179,10 +179,14 @@ exports.createIssueComment = createIssueComment;
 "use strict";
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.JSON_FILE_PATH = exports.GITHUB_TOKEN = void 0;
+exports.COVERITY_PASSWORD = exports.COVERITY_USERNAME = exports.COVERITY_PROJECT_NAME = exports.COVERITY_URL = exports.JSON_FILE_PATH = exports.GITHUB_TOKEN = void 0;
 const core_1 = __nccwpck_require__(2186);
 exports.GITHUB_TOKEN = (0, core_1.getInput)('github-token');
 exports.JSON_FILE_PATH = (0, core_1.getInput)('json-file-path');
+exports.COVERITY_URL = (0, core_1.getInput)('coverity-url');
+exports.COVERITY_PROJECT_NAME = (0, core_1.getInput)('coverity-project-name');
+exports.COVERITY_USERNAME = (0, core_1.getInput)('coverity-username');
+exports.COVERITY_PASSWORD = (0, core_1.getInput)('coverity-password');
 
 
 /***/ }),
