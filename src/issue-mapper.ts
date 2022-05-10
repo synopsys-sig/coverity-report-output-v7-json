0be422e4f8736128717de824740cc9218f410e1b
shubham pandey <shpandey@synopsys.com>
2022-05-10 21:51:55 +0530

    Added log statements to view the output

--- src/issue-mapper.ts
+++ src/issue-mapper.ts
@@ -35,6 +35,7 @@ export async function mapMatchingMergeKeys(relevantMergeKeys: Set<string>): Prom
   while (offset <= totalRows && mergeKeyToProjectIssue.size < relevantMergeKeys.size) {
     try {
       const covProjectIssues = await apiService.findIssues(COVERITY_PROJECT_NAME, offset, PAGE_SIZE)
+      debug('COVERITY PROJECT ISSUE ERROR FROM COVERITY :'+ covProjectIssues)
       totalRows = covProjectIssues.totalRows
       debug(`Found ${covProjectIssues?.rows.length} potentially matching issues on the server`)
 
