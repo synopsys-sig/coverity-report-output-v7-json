08ea54af814628096dc7c8e3578c9bed2ddbbd65
shubham pandey <shpandey@synopsys.com>
2022-05-11 12:16:13 +0530

    Need to test for project name is correct or not

--- src/issue-mapper.ts
+++ src/issue-mapper.ts
@@ -35,7 +35,7 @@ export async function mapMatchingMergeKeys(relevantMergeKeys: Set<string>): Prom
   while (offset <= totalRows && mergeKeyToProjectIssue.size < relevantMergeKeys.size) {
     try {
       const covProjectIssues = await apiService.findIssues(COVERITY_PROJECT_NAME, offset, PAGE_SIZE)
-      info('Checking Coverity server for PROject  NAME...' + covProjectIssues)
+      info('Checking Coverity server for PROject  NAME...' + covProjectIssues.totalRows)
       totalRows = covProjectIssues.totalRows
       debug(`Found ${covProjectIssues?.rows.length} potentially matching issues on the server`)
 
